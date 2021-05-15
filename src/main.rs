d70fba5a23184750839357991d47451627858173
Elias Acevedo <eliasjoseacevedo61@gmail.com>
2021-05-14 23:58:03 -0400

    definiendo pruebas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    #[test]
+    fn test_parameter_text_length_greater_than_one() {}
+
+    #[test]
+    fn test_parameter_column_greater_than_one() {}
+
+}
