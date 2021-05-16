83b1b95466bcd78f9991a9c04c2ec67b50455d58
Elias Acevedo <eliasjoseacevedo61@gmail.com>
2021-05-16 19:59:46 -0400

    Funcion principal completa

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,31 @@ fn main() {
 }
 
 fn principal(texto: &str, limite: usize) -> String {
-    if (limite <= 0) {
+    if limite <= 0 {
         return String::from("");
     }
     
     let mut resultado = String::from("");
+    let mut texto_limitado = soporte(texto, limite as i32);
+
+    while match texto_limitado.1 {
+        Some(texto) => {
+            resultado.push_str(format!("\n{}", texto_limitado.0));
+            texto_limitado = soporte(texto, limite as i32);
+            true
+        }
+        None => {
+            resultado.push_str(texto_limitado.0);
+            false
+        }
+    }{}
+
+    return resultado;
+}
+
+fn soporte(texto: &str, limite: i32) -> (&str, Option<&str>) {
+    let mut resultado = ("", None);
+
     return resultado;
 }
 
