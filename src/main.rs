68a01999790792488e7369af6bd5dfe262466b61
Elias Acevedo <eliasjoseacevedo61@gmail.com>
2021-05-16 19:52:25 -0400

    Prueba inicial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn principal(texto: &str, limite: usize) -> String {
+    if (limite <= 0) {
+        return String::from("");
+    }
+    
+    let mut resultado = String::from("");
+    return resultado;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn probar_longitud_limite() {
+        let texto_prueba = "Este texto es una mera prueba para el ejercicio de hoy";
+        let resultado = principal(texto_prueba, 0);
+        assert!(resultado.len() == 0, "No debe retornar ningun texto")
+    }
+}
