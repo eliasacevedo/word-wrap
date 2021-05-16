8631cc0efc55c8412a351cbdc97a817f0e9fce5c
Elias Acevedo <eliasjoseacevedo61@gmail.com>
2021-05-16 02:26:10 -0400

    agregando funcion soporte

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", envoltorio("Este es un texto de ejemplo que sera utilizado en la segunda grabacion del Kata", 1));
 }
 
-fn envoltorio(texto: &str, columna: usize) -> String {
+fn envoltorio(texto: &str, columna: i32) -> String {
     if columna == 0 {
         return String::from("");
     }
@@ -26,10 +26,27 @@ fn envoltorio(texto: &str, columna: usize) -> String {
     return texto_apilado;
 }
 
-fn funcion_soporte(texto: &str, columna: usize) -> (&str, Option<&str>) {
-    let mut resultado: (&str, Option<&str>) = ("", None);
+fn funcion_soporte(texto: &str, columna: i32) -> (&str, Option<&str>) {
+    if (texto.len() as i32) < columna {
+        return (texto, None);
+    }
 
-    return resultado;
+    let mut indice_actual = columna - 1;
+    loop {
+        if indice_actual == -1{
+            return (texto, None);
+        }
+
+        let letra = texto.chars().nth(indice_actual as usize);
+        if letra == Some(' ') {
+            return (
+                &texto[..(indice_actual as usize)],
+                Some(&texto[(indice_actual as usize) + 1 ..])
+            )
+        }
+
+        indice_actual -= 1;
+    }
 }
 
 #[cfg(test)]
